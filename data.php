6fba4ccd99cb3165caf1b368cf3fae286f8bf37a
Olman Castro J <ocastroj@hotmail.com>
2020-04-19 16:40:04 -0600

    Tres graficos de linea

--- data.php
+++ data.php
@@ -1,5 +1,8 @@
 <?php
 
+// **************************
+// UTILIDADES
+// **************************
 
 // Crea una conexion a la base de datos y ejecuta el query
 function getDataset($query){
@@ -22,39 +25,35 @@ function getDataset($query){
 }
 
 
-// Retorna un dataset con los casos confirmados por continente por dia
-function getConfirmados_x_Continente_Diarios() {
 
-    $sql = "SELECT continentes.nombre as Continent, confirmados.Date as Date, sum(confirmados.Cases) as Cases
-    FROM confirmados 
-    INNER JOIN continentes ON confirmados.Continent = continentes.codigo
-    GROUP BY continentes.nombre, confirmados.Date";
-    
-    $result = getDataset($sql);
+// **************************
+// CALCULO DE DATOS PARA GRAFICOS DE LINEAS
+// **************************
+
+// Agrega un elemento al array de valores si la llave existe y si no, crea una nueva key y elemento
+function update_keypair($arr, $key, $val)
+{
+   if(empty($arr[$key])) $arr[$key] = array($val);
+   else $arr[$key][] = $val;
+   return $arr;
+}
+
+
+// Retorna un dataset con los casos  por continente por dia
+function getDatos_x_Continente_Diarios($query) {
+
+    $result = getDataset($query);
 
     if ($result->num_rows > 0) {
         // output data of each row
 
         $arr = array();
         while($row = $result->fetch_assoc()) {
-            // $arr = array (
-            //     'name' => $row['Continent'],
-            //     'data' => array_map('intval', explode(",",$row['Cases']))
-            // );
-
-            // $series_array[] = $arr;
             $key = $row['Continent'];
-
             $just_date = strtotime($row['Date']) * 1000;
-            //strtotime($row['Date']);
-            //$strip = $date->format('Y-m-d');
-        // $converted_date = date()
             $value = array($just_date, intval($row['Cases']));
-            //$value = $row['Cases'];
             $arr = update_keypair($arr, $key, $value);
-
         }
-
 
         foreach($arr as $key => $value){
             $json[] = array (
@@ -70,13 +69,29 @@ function getConfirmados_x_Continente_Diarios() {
     }
 }
 
-
-// Agrega un elemento al array de valores si la llave existe y si no, crea una nueva key y elemento
-function update_keypair($arr, $key, $val)
-{
-   if(empty($arr[$key])) $arr[$key] = array($val);
-   else $arr[$key][] = $val;
-   return $arr;
+function getConfirmados_x_Continente_Diarios() {
+    $sql = "SELECT continentes.nombre as Continent, confirmados.Date as Date, SUM(confirmados.Cases) as Cases
+    FROM confirmados 
+    INNER JOIN continentes ON confirmados.Continent = continentes.codigo
+    GROUP BY continentes.nombre, confirmados.Date";
+    return getDatos_x_Continente_Diarios($sql);
 }
+
+function getFallecidos_x_Continente_Diarios() {
+    $sql = "SELECT continentes.nombre as Continent, fallecidos.Date as Date, SUM(fallecidos.Cases) as Cases
+    FROM fallecidos 
+    INNER JOIN continentes ON fallecidos.Continent = continentes.codigo
+    GROUP BY continentes.nombre, fallecidos.Date";
+    return getDatos_x_Continente_Diarios($sql);
+}
+
+function getRecuperados_x_Continente_Diarios() {
+    $sql = "SELECT continentes.nombre as Continent, recuperados.Date as Date, SUM(recuperados.Cases) as Cases
+    FROM recuperados 
+    INNER JOIN continentes ON recuperados.Continent = continentes.codigo
+    GROUP BY continentes.nombre, recuperados.Date";
+    return getDatos_x_Continente_Diarios($sql);
+}
+
 
 ?> 
--- line_basic.php
+++ line_basic.php
@@ -1,7 +1,10 @@
 <?php
   include('data.php');
 
-  $json_data = getConfirmados_x_Continente_Diarios();
+  // Obtenemos los datasets para los graficos de linea
+  $line_confirmados_data = getConfirmados_x_Continente_Diarios();
+  $line_fallecidos_data = getFallecidos_x_Continente_Diarios();
+  $line_recuperados_data = getRecuperados_x_Continente_Diarios();
 ?>
 
 <!DOCTYPE HTML>
@@ -11,6 +14,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Proyecto Cenfotec - Vis COVID-19</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+        
 	</head>
 	<body>
 
@@ -19,14 +24,39 @@
         <script src="code/modules/exporting.js"></script>
         <script src="code/modules/export-data.js"></script>
         <script src="code/modules/accessibility.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
-        <figure class="highcharts-figure">
-            <div id="container"></div>
-        </figure>
+        <section class="section">
+            <div class="container">
+
+                <div class="columns">
+
+                    <div class="column">
+                        <figure class="highcharts-figure">
+                            <div id="line-confirmados-region"></div>
+                        </figure>
+                    </div>
+
+                    <div class="column">
+                        <figure class="highcharts-figure1">
+                            <div id="line-fallecidos-region"></div>
+                        </figure>
+                    </div>
+
+                    <div class="column">
+                        <figure class="highcharts-figure2">
+                            <div id="line-recuperados-region"></div>
+                        </figure>
+                    </div>
+
+                </div>
+            </div>   
+        </section>
+
 
 
         <script type="text/javascript">
-            Highcharts.chart('container', {
+            Highcharts.chart('line-confirmados-region', {
 
                 title: {
                     text: 'Números de casos confirmados por región'
@@ -63,7 +93,7 @@
                     }
                 },
 
-                series: <?=$json_data?>
+                series: <?=$line_confirmados_data?>
                 ,
 
                 responsive: {
@@ -82,6 +112,121 @@
                 }
 
             });
+
+            Highcharts.chart('line-fallecidos-region', {
+
+                title: {
+                    text: 'Números de casos fallecidos por región'
+                },
+
+                subtitle: {
+                    text: 'Fuente: Jonh Hopkins Univesity'
+                },
+
+                yAxis: {
+                    title: {
+                        text: 'Casos'
+                    }
+                },
+
+                xAxis: {
+                    accessibility: {
+                        rangeDescription: 'Range: 2010 to 2017'
+                    },
+                    type: 'datetime'
+                },
+
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+
+                plotOptions: {
+                    series: {
+                        label: {
+                            connectorAllowed: false
+                        }
+                    }
+                },
+
+                series: <?=$line_fallecidos_data?>
+                ,
+
+                responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            legend: {
+                                layout: 'horizontal',
+                                align: 'center',
+                                verticalAlign: 'bottom'
+                            }
+                        }
+                    }]
+                }
+
+                });
+
+
+                Highcharts.chart('line-recuperados-region', {
+
+                    title: {
+                        text: 'Números de casos recuperados por región'
+                    },
+
+                    subtitle: {
+                        text: 'Fuente: Jonh Hopkins Univesity'
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: 'Casos'
+                        }
+                    },
+
+                    xAxis: {
+                        accessibility: {
+                            rangeDescription: 'Range: 2010 to 2017'
+                        },
+                        type: 'datetime'
+                    },
+
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
+                    },
+
+                    plotOptions: {
+                        series: {
+                            label: {
+                                connectorAllowed: false
+                            }
+                        }
+                    },
+
+                    series: <?=$line_recuperados_data?>
+                    ,
+
+                    responsive: {
+                        rules: [{
+                            condition: {
+                                maxWidth: 500
+                            },
+                            chartOptions: {
+                                legend: {
+                                    layout: 'horizontal',
+                                    align: 'center',
+                                    verticalAlign: 'bottom'
+                                }
+                            }
+                        }]
+                    }
+
+                    });
 		</script>
 	</body>
 </html>
